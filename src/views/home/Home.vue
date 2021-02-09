ebdcfea7fe77dc839766cde86142e7eb74795ebf
ling meng <meng.ling@changhong.com>
2021-02-09 11:37:34 +0800

    项目大致结构

---
+++ src/views/home/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="home">
+    <nav-bar>
+      <template v-slot:center>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from '../../components/commom/navBar/NavBar';
+import {getHomeMultiData} from '../../network/home';
+
+export default {
+  name: 'Home',
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      banners: {},
+      recommends: {},
+    }
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      this.banners = res.data.banner;//涉及到回收机制
+      this.recommends = res.data.recommend;
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
